6486d40e238c68bfa8fe19b52b9f7c0c9814dae3
Eldar Abdu <125745767+tipscrips@users.noreply.github.com>
2023-07-22 13:36:18 +0300

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,56 +1,56 @@
 function createCalendar(elem, year, month) {
-  let days = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
-
-  let mon = month - 1;
-  let d = new Date(year, mon);
-
-  let table = document.createElement("table");
-  let mainTr = document.createElement("tr");
+  const days = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
+  const mon = month - 1;
+  const date = new Date(year, mon);
+  const table = document.createElement("table");
+  const headTr = document.createElement("tr");
   let tr = document.createElement("tr");
-  table.append(mainTr);
+  table.append(headTr);
   table.append(tr);
 
-  for (let i = 0; i < 7; i++) {
-    let th = document.createElement("th");
-    th.textContent = days[i];
-    mainTr.append(th);
+  for (let day of days) {
+    const th = document.createElement("th");
+    th.textContent = day;
+    headTr.append(th);
   }
 
-  for (let i = 0; i < getDay(d); i++) {
-    let td = document.createElement("td");
+  for (let i = 0; i < getDay(date); i++) {
+    const td = document.createElement("td");
     tr.append(td);
   }
 
-  while (d.getMonth() == mon) {
-    let td = document.createElement("td");
-    td.append(d.getDate());
+  while (date.getMonth() === mon) {
+    const d = getDay(date);
+    const td = document.createElement("td");
+    td.append(date.getDate());
     tr.append(td);
 
-    if (getDay(d) == 6) {
-      // вс, последний день - перевод строки
+    if (d === 6) {
       tr = document.createElement("tr");
       table.append(tr);
     }
 
-    d.setDate(d.getDate() + 1);
+    date.setDate(date.getDate() + 1);
   }
 
-  if (getDay(d) != 0) {
-    for (let i = getDay(d); i < 7; i++) {
-      let td = document.createElement("td");
+  if (getDay(date) != 0) {
+    for (let i = getDay(date); i < 7; i++) {
+      const td = document.createElement("td");
       tr.append(td);
     }
   }
 
-  document.body.append(table);
+  elem.append(table);
 }
 
-function getDay(date) {
-  // получить номер дня недели, от 0 (пн) до 6 (вс)
-  let day = date.getDay();
-  if (day == 0) day = 7; // сделать воскресенье (0) последним днем
-  return day - 1;
+function getDay(d) {
+  let day = d.getDay();
+  if (day === 0) {
+    day = 7;
+  }
+  return --day;
 }
+
 
 let calendar = document.querySelector(".calendar");
 createCalendar(calendar, 2023, 7);
